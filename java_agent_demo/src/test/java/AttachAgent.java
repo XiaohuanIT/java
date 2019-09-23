f1765837d9fce001b6939145711af9876fe1a618
yangxiaohuan <xyang1@xiaohongshu.com>
2019-09-23 20:23:22 +0800

    修改build.gradle，确保生成的jar包包含javassist的依赖

--- java_agent_demo/src/test/java/AttachAgent.java
+++ java_agent_demo/src/test/java/AttachAgent.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class AttachAgent {
   public static void main(String[] args) throws Exception{
     // 这里是 需要 热部署的 java进程的id，运行 jps命令即可知道。
-    VirtualMachine vm = VirtualMachine.attach("55094");//注意，换乘PersonTest的进程id
+    VirtualMachine vm = VirtualMachine.attach("10063");//注意，换乘PersonTest的进程id
     vm.loadAgent("/Users/yangxiaohuan/my_private/github/my_public/java/java_agent_demo/build/libs/java_agent_demo-1.0-SNAPSHOT.jar");
     /*
     while (true) {
