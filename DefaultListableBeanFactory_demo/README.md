3fb42637b500a8d1195e5d1064b4d26160e7ee19
yangxiaohuan <xyang1@xiaohongshu.com>
2020-11-18 06:42:55 +0800

    DefaultListableBeanFactory 和 RootBeanDefinition

---
+++ DefaultListableBeanFactory_demo/README.md
@@ -0,0 +1,8 @@
+
+DefaultListableBeanFactory 获取bean。
+
+RootBeanDefinition 注册 bean。
+
+
+
+Test2.java 中有关于 RootBeanDefinition 的用法。
--- DefaultListableBeanFactory_demo/src/test/java/Test2.java
+++ DefaultListableBeanFactory_demo/src/test/java/Test2.java
@@ -1,5 +1,52 @@
-package PACKAGE_NAME;/**
-* @Author: xiaohuan
-* @Date: 2020/11/15 18:44
-*/public class Test2 {
+import org.junit.Test;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.ResolvableType;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @Author: xiaohuan
+ * @Date: 2020/11/15 18:44
+ */
+public class Test2 {
+
+	@Test
+	public void testGenericsBasedInjectionWithBeanDefinitionTargetResolvableType() {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		RootBeanDefinition bd = new RootBeanDefinition();
+		bd.setInstanceSupplier(TypedFactoryBean::new);
+		bd.setTargetType(ResolvableType.forClassWithGenerics(FactoryBean.class, String.class));
+		bd.setLazyInit(true);
+		context.registerBeanDefinition("fb", bd);
+		context.refresh();
+
+		// 获取bean
+		assertEquals(String.class, context.getType("fb"));
+		// 获取factory的工厂类
+		assertEquals(FactoryBean.class, context.getType("&fb"));
+	}
+
+	static class TypedFactoryBean implements FactoryBean<String> {
+
+		public TypedFactoryBean() {
+			throw new IllegalStateException();
+		}
+
+		@Override
+		public String getObject() {
+			return "";
+		}
+
+		@Override
+		public Class<?> getObjectType() {
+			return String.class;
+		}
+
+		@Override
+		public boolean isSingleton() {
+			return true;
+		}
+	}
 }
