cf908d5b5a6d18c24cb35f9e3f7f158da83438a9
xiaohuan <582901377@qq.com>
2021-07-11 20:41:05 +0800

    CyclicBarrier与CountDownLatch

---
+++ springboot_threadpool_demo/src/main/java/com/xiaohuan/CountDownLatch_demo/CountDownLatchDemo1.java
@@ -0,0 +1,40 @@
+package com.xiaohuan.CountDownLatch_demo;
+
+import java.util.concurrent.*;
+
+public class CountDownLatchDemo1 {
+    public static void main(String[] args) {
+        BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(10);
+        ExecutorService pool = new ThreadPoolExecutor(5, 10, 5,
+                TimeUnit.SECONDS, queue, new ThreadPoolExecutor.AbortPolicy());
+        int size = 5;
+        // 设置参数时，线程实际执行数size+1，将main线程也加到等待队列中
+        final CountDownLatch latch = new CountDownLatch(size);
+
+        for (int i = 0; i < size; i++) {
+            int index = i;
+            pool.submit(() -> {
+                try {
+                    System.out.println("子线程" + Thread.currentThread().getName() + "正在赶路"+index);
+                    TimeUnit.SECONDS.sleep(index);
+                    System.out.println("子线程" + Thread.currentThread().getName() + "到公司了"+index);
+                    //调用latch的countDown方法使计数器-1
+                    latch.countDown();
+                    System.out.println("子线程" + Thread.currentThread().getName() + "开始工作"+index);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+        try {
+            System.out.println("门卫等待员工上班中...");
+            //主线程阻塞等待计数器归零
+            latch.await();
+            System.out.println("员工都来了,门卫去休息了");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ springboot_threadpool_demo/src/main/java/com/xiaohuan/cyclicbarrier_demo/CyclicBarrierDemo1.java
@@ -0,0 +1,34 @@
+package com.xiaohuan.cyclicbarrier_demo;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class CyclicBarrierDemo1 {
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        int size = 5;
+        // 设置参数时，线程实际执行数size+1，将main线程也加到等待队列中
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(size,()->{
+            System.out.println("所有人都准备好了裁判开始了");});
+
+            for (int i = 0; i < size; i++) {
+            int index = i;
+            pool.submit(() -> {
+                try {
+                    System.out.println("子线程" + Thread.currentThread().getName() + "正在准备"+index);
+                    TimeUnit.SECONDS.sleep(index);
+                    System.out.println("子线程" + Thread.currentThread().getName() + "准备好了"+index);
+                    cyclicBarrier.await();
+                    System.out.println("子线程" + Thread.currentThread().getName() + "开始跑了"+index);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
+    }
+
+
+}
